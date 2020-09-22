b7dc1174cbdf9269336668e791cc820bfe56ea08
Glitch (hello-webpage) <none>
2020-09-22 11:36:36 +0000

    🌞📉 Checkpoint ./script.js:22133009/2165 ./index.html:22133009/2484 ./style.css:22133009/1190

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+$(document).ready(function(){
+    // Level 1 Responsive Menu
+    $(".has_child").click(function(){
+        var id = $(this).find("ul");
+ if((id).css('display')=='none'){
+ (id).css({'display':'block'});
+ }else{
+ (id).css({'display':'none'});
+ } 
+    });
+});
+ 
+// Add active class to active link
+$(function(){
+ var pathname = (window.location.pathname.match(/[^\/]+$/)[0]);
+ $('nav ul li a').each(function() {
+    var url = $(this).attr('href');
+    var url_new = url.split("#");
+    var new_url = url_new[0]
+if (new_url == pathname){
+    $(this).addClass('active');
+    }
+ });
+});
